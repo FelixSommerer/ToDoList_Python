f60a6a1068d125e579f442ef7783e320ceebce81
Felix Sommerer <sommeref@dhbw-loerrach.de>
2020-09-30 18:19:34 +0200

    30.09.20

--- test.py
+++ test.py
@@ -1,28 +1,54 @@
 from tkinter import *
 
+from PIL import Image, ImageTk
+
 import login
+import mainPage
+import register
 
 
 class App:
 
-    def __init__(self, master):
-        self.frame = Frame(master)
-        self.frame.grid()
+    def __init__(self):
+        root = Tk()
+        root.geometry('1280x720')
+        root.wm_iconbitmap('toDo_Logo.ico')
+        root.winfo_toplevel().title("To-do-Liste")
+        self.frame = Frame()
+        self.frame.pack()
 
-        self.button = Button(self.frame, text="QUIT", fg="red", command=self.frame.quit)
-        self.button.grid(row=0,column=0)
+        logo = Image.open('toDo_Logo.png')
+        logo = logo.resize((225, 225), Image.ANTIALIAS)
+        logoTk = ImageTk.PhotoImage(logo)
 
-        self.hi_there = Button(self.frame, text="Hello", command=lambda: self.create_frame2(master))
-        self.hi_there.grid(row=0,column=1)
+        self.image_label = Label(self.frame, image=logoTk)
+        self.image_label.image = logoTk
+        self.welcome_text = Label(self.frame, text='Willkommen bei der ToDo-Liste', font=('Calibri', 28))
+        self.go_to_register = Button(self.frame, text='Registrieren', font=('Calibri', 15),
+                                     command=lambda: self.change_frame_register(root))
+        self.go_to_login = Button(self.frame, text='Login', font=('Calibri', 15),
+                                  command=lambda: self.change_frame_login(root))
 
+        self.image_label.pack(pady=60)
+        self.welcome_text.pack()
+        self.go_to_register.pack(side=LEFT, pady=60)
+        self.go_to_register.config(width=15, height=1)
+        self.go_to_login.pack(side=RIGHT, pady=60)
+        self.go_to_login.config(width=15, height=1)
 
-    def create_frame2(self, master):
+        root.mainloop()
+
+    def change_frame_login(self, root):
+        self.frame.pack_forget()
+        login.Login(root)
+
+    def change_frame_register(self, root):
+        self.frame.pack_forget()
+        register.Register(root)
+
+    def change_frame_main(self, root):
         self.frame.grid_forget()
-        #login.Login(master)
+        mainPage.Main(root, 1, 'headline', 0, 0)
 
 
-root = Tk()
-
-app2 = App(root)
-
-root.mainloop()
+app = App()
--- welcome.py
+++ welcome.py
@@ -1,5 +1,5 @@
 from tkinter import *
-#from PIL import Image, ImageTk
+from PIL import Image, ImageTk
 import login
 import mainPage
 import register
@@ -13,52 +13,40 @@ class Welcome:
         root.wm_iconbitmap('toDo_Logo.ico')
         root.winfo_toplevel().title("To-do-Liste")
         self.frame = Frame()
-        self.frame.grid()
-        self.frame.columnconfigure((0,1,2), weight=2)
-       # self.frame.rowconfigure((0), weight=1)
+        self.frame.pack()
 
-        """logo = Image.open('toDo_Logo.png')
+        logo = Image.open('toDo_Logo.png')
         logo = logo.resize((225, 225), Image.ANTIALIAS)
-        logoTk = ImageTk.PhotoImage(logo)"""
+        logoTk = ImageTk.PhotoImage(logo)
 
-        """self.image_label = Label(self.frame, image=logoTk)
-        self.image_label.image = logoTk"""
+        self.image_label = Label(self.frame, image=logoTk)
+        self.image_label.image = logoTk
         self.welcome_text = Label(self.frame, text='Willkommen bei der ToDo-Liste', font=('Calibri', 28))
-        self.go_to_register = Button(self.frame, text='Registrieren', font=('Calibri', 15), command=lambda: self.change_frame_register(root))
-        self.go_to_login = Button(self.frame, text='Login', font=('Calibri', 15), command=lambda: self.change_frame_login(root))
+        self.go_to_register = Button(self.frame, text='Registrieren', font=('Calibri', 15),
+                                     command=lambda: self.change_frame_register(root))
+        self.go_to_login = Button(self.frame, text='Login', font=('Calibri', 15),
+                                  command=lambda: self.change_frame_login(root))
+
+        self.image_label.pack(pady=60)
+        self.welcome_text.pack()
+        self.go_to_register.pack(side=LEFT, pady=60)
+        self.go_to_login.pack(side=RIGHT, pady=60)
 
 
-        #self.image_label.grid_columnconfigure(0, weight=1)
-        #self.image_label.grid_rowconfigure(0, weight=1)
-        #self.image_label.grid(row=0, column=1, columnspan=2, pady=60)
-        #self.image_label.place( relx=0.5, rely=0.5, anchor='center')
-        #self.welcome_text.grid_columnconfigure(0, weight=1)
-        #self.welcome_text.grid_rowconfigure(1, )
-        self.welcome_text.grid(row=1, column=0, sticky='nesw')
-        #self.go_to_register.grid_columnconfigure(0, weight=1)
-        #self.go_to_register.grid_rowconfigure(2, weight=1)
-        self.go_to_register.grid(row=2, column=0, pady=65)
-        self.go_to_register.config(width=15, height=1)
-        #self.go_to_login.grid_columnconfigure(1, weight=1)
-        #self.go_to_login.grid_rowconfigure(2, weight=1)
-        self.go_to_login.grid(row=2, column=2, pady=65, sticky='e')
         self.go_to_login.config(width=15, height=1)
+        self.go_to_register.config(width=15, height=1)
 
-        #self.frame.rowconfigure(1,weight=1)
 
         root.mainloop()
 
     def change_frame_login(self, root):
-        self.frame.grid_forget()
+        self.frame.pack_forget()
         login.Login(root)
 
     def change_frame_register(self, root):
-        self.frame.grid_forget()
+        self.frame.pack_forget()
         register.Register(root)
 
-    def change_frame_main(self, root):
-        self.frame.grid_forget()
-        mainPage.Main(root, 1, 'headline', 0, 0)
 
 
 app = Welcome()
